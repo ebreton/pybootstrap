3b550a4c65e2c67393bd17c2a23808439c20dc87
ebreton <email>
2018-09-30 21:02:00 +0200

    Prepared release 0.2.1

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.2.0-4-g98b27b8'
+_release = '0.2.0-5-g31e935e'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.2.1'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '98b27b8fad8127f6602194ecb6a2add4516ae637'
+_build = '31e935eb706d572062b8bc8c3c1d09012444c09b'
